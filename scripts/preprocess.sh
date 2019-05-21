b32aca256b2ed244cac03d13c8279696a7398acb
amine1992 <amin.nouira@gmail.com>
2019-05-21 01:12:32 +0100

    add preprocessing scripts

---
+++ scripts/preprocess.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -u
+
+echo Starting local TFT preprocessing...
+
+# Preprocess the train files, keeping the transform functions
+echo Preprocessing train data...
+rm -R -f ./data/train/bookings_output
+python preprocess.py \
+  --input ./data/train/train.csv \
+  --schema_file ./data/tfdv_output/schema.pbtxt \
+  --output_dir ./data/train/bookings_output \
+  --outfile_prefix train_transformed \
+  --runner DirectRunner
+
+# Preprocess the eval files
+echo Preprocessing eval data...
+rm -R -f ./data/eval/bookings_output
+python preprocess.py \
+  --input ./data/eval/eval.csv \
+  --schema_file ./data/tfdv_output/schema.pbtxt \
+  --output_dir ./data/eval/bookings_output \
+  --outfile_prefix eval_transformed \
+  --transform_dir ./data/train/bookings_output \
+  --runner DirectRunner
