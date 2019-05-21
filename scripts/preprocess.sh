f75ee821f2f86397288278398fd7c71da1fb234f
amine1992 <amin.nouira@gmail.com>
2019-05-21 01:15:18 +0100

    fix data path

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -5,21 +5,21 @@ echo Starting local TFT preprocessing...
 
 # Preprocess the train files, keeping the transform functions
 echo Preprocessing train data...
-rm -R -f ./data/train/bookings_output
+rm -R -f ../data/train/bookings_output
 python preprocess.py \
-  --input ./data/train/train.csv \
-  --schema_file ./data/tfdv_output/schema.pbtxt \
-  --output_dir ./data/train/bookings_output \
+  --input ../data/train/train.csv \
+  --schema_file ../data/tfdv_output/schema.pbtxt \
+  --output_dir ../data/train/bookings_output \
   --outfile_prefix train_transformed \
   --runner DirectRunner
 
 # Preprocess the eval files
 echo Preprocessing eval data...
-rm -R -f ./data/eval/bookings_output
+rm -R -f ../data/eval/bookings_output
 python preprocess.py \
-  --input ./data/eval/eval.csv \
-  --schema_file ./data/tfdv_output/schema.pbtxt \
-  --output_dir ./data/eval/bookings_output \
+  --input ../data/eval/eval.csv \
+  --schema_file ../data/tfdv_output/schema.pbtxt \
+  --output_dir ../data/eval/bookings_output \
   --outfile_prefix eval_transformed \
-  --transform_dir ./data/train/bookings_output \
+  --transform_dir ../data/train/bookings_output \
   --runner DirectRunner
